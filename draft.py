863129f7cf434bde1b9a7846798343ad011add5f
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-09 18:47:03 +0300

    lesson 02 task 06

--- draft.py
+++ draft.py
@@ -1,36 +1,48 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# есть список животных в зоопарке
+# Есть список песен группы Depeche Mode со временем звучания с точносттю до долей минут
 
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
 
-# посадите медведя (bear) между львом и кенгуру
-#  и выведите список на консоль
+# распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+#   Три песни звучат ХХХ минут
+# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+
 # TODO здесь ваш код
 
-zoo.insert(1, 'bear')
-print(*zoo)
+minutes = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
+print("Три песни звучат", round(minutes, 2), "минут")
 
-# добавьте птиц из списка birds в последние клетки зоопарка
-birds = ['rooster', 'ostrich', 'lark', ]
-#  и выведите список на консоль
+# Есть словарь песен группы Depeche Mode
+violator_songs_dict = {
+    'World in My Eyes': 4.76,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+
+# распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
+#   А другие три песни звучат ХХХ минут
+
 # TODO здесь ваш код
 
-zoo.extend(birds)
-print(*zoo)
-
-# уберите слона
-#  и выведите список на консоль
-# TODO здесь ваш код
-
-zoo.remove('elephant')
-print(*zoo)
-
-# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
-# Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
-
-print(zoo.index('lion') + 1)
-print(zoo.index('lark') + 1)
-
+minutes_2 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth']\
+            + violator_songs_dict['Blue Dress']
+print('А другие три песни звучат', round(minutes_2, 2), 'минут')
--- lesson002/06_songs_list.py
+++ lesson002/06_songs_list.py
@@ -22,6 +22,9 @@ violator_songs_list = [
 
 # TODO здесь ваш код
 
+minutes = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
+print("Три песни звучат", round(minutes, 2), "минут")
+
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,
@@ -39,3 +42,7 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+
+minutes_2 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth']\
+            + violator_songs_dict['Blue Dress']
+print('А другие три песни звучат', round(minutes_2, 2), 'минут')
