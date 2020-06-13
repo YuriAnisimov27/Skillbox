764d0cc62c2bfefd1034fde7a50d56656d23d004
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-14 02:37:46 +0300

    lesson 03 task 01

--- lesson003/01_days_in_month.py
+++ lesson003/01_days_in_month.py
@@ -7,8 +7,25 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+# user_input = input("Введите, пожалуйста, номер месяца: ")
+# month = int(user_input)
+# print('Вы ввели', month)
 
 # TODO здесь ваш код
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+
+month_with_31_days = [1, 3, 5, 7, 8, 10, 12]
+month_with_30_days = [4, 6, 9, 11]
+month_with_28_days = [2]
+if month in month_with_31_days:
+    days = 31
+elif month in month_with_30_days:
+    days = 30
+elif month in month_with_28_days:
+    days = 28
+else:
+    print('номер месяца некорректен')
+    days = 'непонятно сколько'
+
+print(f'вы ввели {month}, в этом месяце {days} дней')
