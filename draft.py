13ce4a333171576b76d05c3ef3e0fb8bfa4132b2
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-14 03:28:20 +0300

    lesson 03 task 04

--- draft.py
+++ draft.py
@@ -1,8 +1,6 @@
-a, b = 2, 1
-
-answer = 0
-counter = b
-while counter <= a:
-    counter += b
-    answer += 1
-print(f'Целочисленное деление {a} на {b} дает {answer}')
+educational_grant, expenses = 10000, 12000
+need_money = 0
+for i in range(10):
+    need_money += expenses - educational_grant
+    expenses *= 1.03
+print(f'Студенту надо попросить {round(need_money, 2)} рублей')
--- lesson003/04_student.py
+++ lesson003/04_student.py
@@ -12,3 +12,9 @@
 educational_grant, expenses = 10000, 12000
 
 # TODO здесь ваш код
+
+need_money = 0
+for i in range(10):
+    need_money += expenses - educational_grant
+    expenses *= 1.03
+print(f'Студенту надо попросить {round(need_money, 2)} рублей')
