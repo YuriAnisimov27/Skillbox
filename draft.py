d1b713d5b9e7a046a160d529dc8299d06c4c6b30
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-16 23:26:56 +0300

    Added tasks from module 4

--- draft.py
+++ draft.py
@@ -1,25 +1,2 @@
-import simple_draw
-
-simple_draw.resolution = (1300, 700)
-
-
-def smily_face(x, y, colore):
-    point_center = simple_draw.get_point(x, y)
-    left_eye = simple_draw.get_point(x - 40, y + 27)
-    right_eye = simple_draw.get_point(x + 40, y + 27)
-
-    simple_draw.circle(point_center, 88, color=colore, width=4)
-    simple_draw.circle(left_eye, 23, color=colore, width=4)
-    simple_draw.circle(right_eye, 23, color=colore, width=4)
-
-    for i in range(10):
-        new_center = simple_draw.get_point(x, y - i * 3)
-        simple_draw.circle(new_center, 20 - i, width=0)
-
-
-for _ in range(10):
-    x = simple_draw.random_number(0, 1300)
-    y = simple_draw.random_number(0, 700)
-    smily_face(x, y, simple_draw.random_color())
-
-simple_draw.pause()
+litera = True
+print(isinstance(litera, bool))
