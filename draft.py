a5f59a0ecbb6c36675c7b3fe2d19012987ad4938
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-23 14:48:43 +0300

    lesson 04 task 03

--- draft.py
+++ draft.py
@@ -3,13 +3,13 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 
-def draw_figure(start_point, end_point, angles_count, line_length, color):
+def draw_figure(start_point, end_point, angles_count, line_length):
     point = sd.get_point(start_point, end_point)
     angle = 360 // angles_count
 
     for i in range(angles_count):
         point_draw = sd.get_vector(start_point=point, angle=i * angle, length=line_length)
-        point_draw.draw(color=color, width=3)
+        point_draw.draw()
         point = point_draw.end_point
 
 
@@ -27,50 +27,16 @@ def draw_figure(start_point, end_point, angles_count, line_length, color):
 # point6.draw()
 
 
-
-
-colors = {
-    'COLOR_RED': (255, 0, 0),
-    'COLOR_ORANGE': (255, 127, 0),
-    'COLOR_YELLOW': (255, 255, 0),
-    'COLOR_GREEN': (0, 255, 0),
-    'COLOR_CYAN': (0, 255, 255),
-    'COLOR_BLUE': (0, 0, 255),
-    'COLOR_PURPLE': (255, 0, 255),
-    'COLOR_DARK_YELLOW': (127, 127, 0),
-    'COLOR_DARK_ORANGE': (127, 63, 0),
-    'COLOR_DARK_RED': (127, 0, 0),
-    'COLOR_DARK_GREEN': (0, 127, 0),
-    'COLOR_DARK_CYAN': (0, 127, 127),
-    'COLOR_DARK_BLUE': (0, 0, 127),
-    'COLOR_DARK_PURPLE': (127, 0, 127),
-}
-
-for i, item in enumerate(colors, start=1):
-    print(i, item)
-my_color = int(input('Введите желаемый цвет > '))
-while my_color < 1 or my_color > 14:
+N = int(input('''Возможные фигуры:
+0. Треугольник
+1. Квадрат
+2. Пятиугольник
+3. Шестиугольник
+Введите желаемую фигуру > 
+'''))
+while N < 0 or N > 3:
     print('Вы ввели некорректный номер!')
-    my_color = int(input('Введите желаемый цвет > '))
+    N = int(input('Введите желаемую фигуру > '))
 
-colors_list = [
-    'COLOR_RED',
-    'COLOR_ORANGE',
-    'COLOR_YELLOW',
-    'COLOR_GREEN',
-    'COLOR_CYAN',
-    'COLOR_BLUE',
-    'COLOR_PURPLE',
-    'COLOR_DARK_YELLOW',
-    'COLOR_DARK_ORANGE',
-    'COLOR_DARK_RED',
-    'COLOR_DARK_GREEN',
-    'COLOR_DARK_CYAN',
-    'COLOR_DARK_BLUE',
-    'COLOR_DARK_PURPLE',
-]
-
-
-
-draw_figure(200, 200, 4, 200, colors[colors_list[my_color - 1]])
+draw_figure(200, 200, N + 3, 200)
 sd.pause()
--- lesson004/03_shape_select.py
+++ lesson004/03_shape_select.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import simple_draw as sd
 
 # Запросить у пользователя желаемую фигуру посредством выбора из существующих
 #   вывести список всех фигур с номерами и ждать ввода номера желаемой фигуры.
@@ -11,4 +10,31 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+import simple_draw as sd
+
+
+sd.resolution = (1200, 600)
+
+def draw_figure(start_point, end_point, angles_count, line_length):
+    point = sd.get_point(start_point, end_point)
+    angle = 360 // angles_count
+
+    for i in range(angles_count):
+        point_draw = sd.get_vector(start_point=point, angle=i * angle, length=line_length)
+        point_draw.draw()
+        point = point_draw.end_point
+
+
+N = int(input('''Возможные фигуры:
+0. Треугольник
+1. Квадрат
+2. Пятиугольник
+3. Шестиугольник
+Введите желаемую фигуру > 
+'''))
+while N < 0 or N > 3:
+    print('Вы ввели некорректный номер!')
+    N = int(input('Введите желаемую фигуру > '))
+
+draw_figure(200, 200, N + 3, 200)
 sd.pause()
