91521ab64d95e7113603597a62c1dcd04f2b28fb
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-06-23 17:18:44 +0300

    lesson 04 task 04

--- draft.py
+++ draft.py
@@ -1,42 +1,22 @@
 import simple_draw as sd
 
-sd.resolution = (1200, 600)
+sd.resolution = (1300, 600)
 
 
-def draw_figure(start_point, end_point, angles_count, line_length):
-    point = sd.get_point(start_point, end_point)
-    angle = 360 // angles_count
-
-    for i in range(angles_count):
-        point_draw = sd.get_vector(start_point=point, angle=i * angle, length=line_length)
-        point_draw.draw()
-        point = point_draw.end_point
-
-
-# point1 = sd.get_vector(start_point=point, angle=0, length=line_length)
-# point1.draw()
-# point2 = sd.get_vector(start_point=point1.end_point, angle=60, length=line_length)
-# point2.draw()
-# point3 = sd.get_vector(start_point=point2.end_point, angle=120, length=line_length)
-# point3.draw()
-# point4 = sd.get_vector(start_point=point3.end_point, angle=180, length=line_length)
-# point4.draw()
-# point5 = sd.get_vector(start_point=point4.end_point, angle=240, length=line_length)
-# point5.draw()
-# point6 = sd.get_vector(start_point=point5.end_point, angle=300, length=line_length)
-# point6.draw()
+def draw_bunches(start_point, angle, length):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=start_point, angle=angle - 15, length=length)
+    v1.draw()
+    v2 = sd.get_vector(start_point=start_point, angle=angle + 15, length=length)
+    v2.draw()
+    length = length * (0.75 * sd.random_number(-20, 20) / 100 + 0.75)
+    angle1 = angle + (30 * sd.random_number(-40, 40) / 100 + 30)
+    angle2 = angle - (30 * sd.random_number(-40, 40) / 100 + 30)
+    draw_bunches(v1.end_point, angle1, length)
+    draw_bunches(v2.end_point, angle2, length)
 
 
-N = int(input('''Возможные фигуры:
-0. Треугольник
-1. Квадрат
-2. Пятиугольник
-3. Шестиугольник
-Введите желаемую фигуру > 
-'''))
-while N < 0 or N > 3:
-    print('Вы ввели некорректный номер!')
-    N = int(input('Введите желаемую фигуру > '))
-
-draw_figure(200, 200, N + 3, 200)
+root_point = sd.get_point(600, 30)
+draw_bunches(start_point=root_point, angle=90, length=160)
 sd.pause()
--- lesson004/04_fractal.py
+++ lesson004/04_fractal.py
@@ -8,12 +8,43 @@ import simple_draw as sd
 # - угол рисования,
 # - длина ветвей,
 # Отклонение ветвей от угла рисования принять 30 градусов,
+sd.resolution = (1200, 600)
+root_point = sd.get_point(300, 0)
 
+
+def draw_branches(start_point, angle, length):
+    v1 = sd.get_vector(start_point=start_point, angle=angle - 30, length=length)
+    v1.draw()
+    v2 = sd.get_vector(start_point=start_point, angle=angle + 30, length=length)
+    v2.draw()
+
+draw_branches(root_point, 90, 100)
 # 2) Сделать draw_branches рекурсивной
 # - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
 # - вызывать саму себя 2 раза из точек-концов нарисованных ветвей,
 #   с параметром "угол рисования" равным углу только что нарисованной ветви,
 #   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
+
+sd.resolution = (1300, 600)
+root_point = sd.get_point(600, 0)
+
+
+def draw_bunches(start_point, angle, length):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=start_point, angle=angle - 15, length=length)
+    v1.draw()
+    v2 = sd.get_vector(start_point=start_point, angle=angle + 15, length=length)
+    v2.draw()
+    length = length * 0.75
+    angle1 = angle + 30
+    angle2 = angle - 30
+    draw_bunches(v1.end_point, angle1, length)
+    draw_bunches(v2.end_point, angle2, length)
+
+draw_bunches(root_point, 90, 160)
+sd.pause()
+
 
 # 3) первоначальный вызов:
 # root_point = get_point(300, 30)
@@ -36,6 +67,24 @@ import simple_draw as sd
 # Пригодятся функции
 # sd.random_number()
 
-sd.pause()
+sd.resolution = (1300, 600)
 
+
+def draw_bunches(start_point, angle, length):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=start_point, angle=angle - 15, length=length)
+    v1.draw()
+    v2 = sd.get_vector(start_point=start_point, angle=angle + 15, length=length)
+    v2.draw()
+    length = length * (0.75 * sd.random_number(-20, 20) / 100 + 0.75)
+    angle1 = angle + (30 * sd.random_number(-40, 40) / 100 + 30)
+    angle2 = angle - (30 * sd.random_number(-40, 40) / 100 + 30)
+    draw_bunches(v1.end_point, angle1, length)
+    draw_bunches(v2.end_point, angle2, length)
+
+
+root_point = sd.get_point(600, 30)
+draw_bunches(start_point=root_point, angle=90, length=160)
+sd.pause()
 
